150f48ebe982b606f0da41ffcac8380828eaeaa5
Aloui-khalil <56514173+Aloui-khalil@users.noreply.github.com>
2020-02-16 16:29:25 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Compdm from './Compdm.jsx';
+import './App.css';
+
+
+function App() {
+  return (
+    <div>
+      <Compdm>
+        
+      </Compdm>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ Compdm.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Component} from 'react';
+
+class Compdm extends Component
+{
+ componentDidMount(){
+    setTimeout( alert("Welcome"), 1000);
+  }
+render(){
+  return(
+    <div>
+      <p>
+        zezezeze
+      </p>
+    </div>
+  )
+}
+}
+
+export default Compdm
